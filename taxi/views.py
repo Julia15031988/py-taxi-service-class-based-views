634606d3dacae17efeedf60f659004bce27670db
Julia Okhrymenko <Ohrimenko15@gmail.com>
2024-11-23 20:41:49 +0200

    'Solution'

--- taxi/views.py
+++ taxi/views.py
@@ -6,9 +6,9 @@ from taxi.models import Driver, Car, Manufacturer
 
 def index(request):
     context = {
-    "num_drivers": Driver.objects.count(),
-    "num_cars": Car.objects.count(),
-    "num_manufacturers": Manufacturer.objects.count(),
+        "num_drivers": Driver.objects.count(),
+        "num_cars": Car.objects.count(),
+        "num_manufacturers": Manufacturer.objects.count()
     }
 
     return render(request, "taxi/index.html", context=context)
@@ -20,12 +20,14 @@ class ManufacturerListView(generic.ListView):
     queryset = Manufacturer.objects.all().order_by("name")
     paginate_by = 5
 
+
 class CarListView(generic.ListView):
     model = Car
     template_name = "taxi/car_list.html"
     context_object_name = "car_list"
     queryset = Car.objects.select_related("manufacturer").all()
     paginate_by = 5
+
 
 class CarDetailView(DetailView):
     model = Car
@@ -40,4 +42,5 @@ class DriverListView(ListView):
 
 class DriverDetailView(DetailView):
     model = Driver
-    queryset = Driver.objects.prefetch_related('car_set__manufacturer').all()
+    queryset = Driver.objects.prefetch_related("car_set__manufacturer").all()
+
